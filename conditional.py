abef4a39a326cf15e66f3963961b16900959cb65
mandanikuldip7-ux <mandanikuldip7@gmail.com>
2026-02-16 11:32:09 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,34 @@
+
+"""age=int(input ("enter your age:"))
+if age>=18:
+    print("yes, you are eligible.")
+else :
+    print("you are not eligible.")"""
+    
+"""marks=int(input("enter your marks:"))
+if marks >=90:
+    print("you will get A grade.")
+elif marks>=70:
+    print("you will get B grade.")
+else :
+    print("yopu will get C grade:")"""
+    
+"""num=int(input("enter your number"))
+if  num %2==0 :
+    print("even number")
+else:
+    print("odd number")"""
+
+"""num=int(input("enter your number:"))
+if num>0 :
+    print("positive number.")
+elif num<0 :
+    print("nagative number.")
+else :
+    print("zero")"""
+
+licence=int(input("enter your age:"))   
+if 18>=:
+    print("yes, you are eligible.")
+else:
+    print("you are not eligible.")
